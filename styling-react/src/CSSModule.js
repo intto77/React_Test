b4b0214f0370869ac5533a1162009768c95861dd
intto77 <twkimtw77@gmail.com>
2022-05-23 14:06:48 +0900

    CSS Module

---
+++ styling-react/src/CSSModule.js
@@ -0,0 +1,11 @@
+﻿import styles from "./CSSModule.module.css";
+
+const CSSModule = () => {
+  return (
+    <div className={`${styles.wrapper} ${styles.inverted}`}>
+      안녕하세요 저는 <span className="something">CSS module</span>
+    </div>
+  );
+};
+
+export default CSSModule;
