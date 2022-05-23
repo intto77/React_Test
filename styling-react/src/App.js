b6dffa6f4179b1e7ffe1ea167d0264e179d830d9
intto77 <twkimtw77@gmail.com>
2022-05-23 11:32:57 +0900

    SassComponent 추가

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,10 +1,14 @@
-function App() {
-  return (
-    <div>
-      <h1>git hub test</h1>
-      <h2> 소스 반영 </h2>
-    </div>
-  );
+import { Component } from "react";
+import SassComponent from "./SassComponent";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
