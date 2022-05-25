1165d53a6fe0bf3df1d90a6f97455a9c6ef97d26
intto77 <twkimtw77@gmail.com>
2022-05-25 14:06:46 +0900

    컴포넌트 스타일링 완료

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,4 +1,14 @@
-function App() {
-  return <div></div>;
-}
+import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
+
+const App = () => {
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList />
+    </TodoTemplate>
+  );
+};
+
 export default App;
