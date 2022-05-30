640268cc69923a8b1b762d2782fefc78b65fbb99
intto77 <twkimtw77@gmail.com>
2022-05-30 10:56:31 +0900

    useState 함수형 업데이트와 immer 함께 쓰기

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -10,17 +10,14 @@ const App = () => {
   });
 
   //input 수정을 위함 함수
-  const onChange = useCallback(
-    (e) => {
-      const { name, value } = e.target;
-      setForm(
-        produce(form, (draft) => {
-          draft[name] = value;
-        })
-      );
-    },
-    [form]
-  );
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setForm(
+      produce((draft) => {
+        draft[name] = value;
+      })
+    );
+  }, []);
 
   //array에 새 항목 등록
   const onSubmit = useCallback(
@@ -34,7 +31,7 @@ const App = () => {
 
       // arrya에 새 항목 등록
       setData(
-        produce(data, (draft) => {
+        produce((draft) => {
           draft.array.push(info);
         })
       );
@@ -50,19 +47,16 @@ const App = () => {
   );
 
   //항목을 삭제하는 함수
-  const onRemove = useCallback(
-    (id) => {
-      setData(
-        produce(data, (draft) => {
-          draft.array.splice(
-            draft.array.findIndex((info) => info.id === id),
-            1
-          );
-        })
-      );
-    },
-    [data]
-  );
+  const onRemove = useCallback((id) => {
+    setData(
+      produce((draft) => {
+        draft.array.splice(
+          draft.array.findIndex((info) => info.id === id),
+          1
+        );
+      })
+    );
+  }, []);
 
   return (
     <div>
