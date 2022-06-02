1b50753f49cb7fc8cd23f9f3fb215e5addb86c61
intto77 <twkimtw77@gmail.com>
2022-06-02 12:26:31 +0900

    데이터 연동하기

--- new-viewer/src/App.js
+++ new-viewer/src/App.js
@@ -1,34 +1,7 @@
-import { useState } from "react";
-import axios from "axios";
+import NewList from "./component/NewsList";
 
-// API key :
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=00248aff774441178ec1d9fcf0a28665",
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          row={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewList />;
 };
 
 export default App;
