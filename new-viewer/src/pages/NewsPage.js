6c438839dae97446b4c32935d70b4a19ed163c4f
intto77 <twkimtw77@gmail.com>
2022-07-19 14:41:15 +0900

    커밋 확인

--- new-viewer/src/pages/NewsPage.js
+++ new-viewer/src/pages/NewsPage.js
@@ -4,7 +4,7 @@ import NewList from "../component/NewsList";
 
 const NewsPage = () => {
   const params = useParams();
-  // 카테고리가 선택되지 않았으면 기본값 all로 사용
+  // 카테고리가 선택되지 않았으면 기본값 all로 사용.
   const category = params.category || "all";
 
   return (
