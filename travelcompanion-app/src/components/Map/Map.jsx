281211d2795f667dd56adef3900a4d439f007087
intto77 <twkimtw77@gmail.com>
2022-07-21 16:21:38 +0900

    폴더 구조 생성

---
+++ travelcompanion-app/src/components/Map/Map.jsx
@@ -0,0 +1,7 @@
+﻿import React from "react";
+
+const Map = () => {
+  return <h1>Map</h1>;
+};
+
+export default Map;
---
+++ travelcompanion-app/src/components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,7 @@
+﻿import React from "react";
+
+const PlaceDetails = () => {
+  return <h1>PlaceDetails</h1>;
+};
+
+export default PlaceDetails;
