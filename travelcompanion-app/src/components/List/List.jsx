5cbe8cfa1cf4f146e55d619477b99bfa05048446
intto77 <twkimtw77@gmail.com>
2022-07-27 13:15:04 +0900

    튜토리얼 완료

--- travelcompanion-app/src/components/List/List.jsx
+++
@@ -1,7 +0,0 @@
-﻿import React from "react";
-
-const List = () => {
-  return <h1>List</h1>;
-};
-
-export default List;
--- travelcompanion-app/src/components/PlaceDetails/PlaceDetails.jsx
+++
@@ -1,7 +0,0 @@
-﻿import React from "react";
-
-const PlaceDetails = () => {
-  return <h1>PlaceDetails</h1>;
-};
-
-export default PlaceDetails;
