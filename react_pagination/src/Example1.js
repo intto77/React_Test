b6cbfb947626dd5640fd88bd9ce64219c883f37b
intto77 <twkimtw77@gmail.com>
2022-07-27 16:13:34 +0900

    작업 완료

---
+++ react_pagination/src/Example1.js
@@ -0,0 +1,34 @@
+﻿import { getPostsPage } from "./api/axios";
+import { useState, useEffect } from "react";
+import Post from "./Post";
+
+const Example1 = () => {
+  const [page, setPage] = useState(1);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPostsPage(page).then((json) => setPosts(json));
+  }, [page]);
+
+  const content = posts.map((post) => <post key={post.id} post={post}></post>);
+
+  const nextPage = () => setPage((prev) => prev + 1);
+
+  const prevPage = () => setPage((prev) => prev - 1);
+
+  return (
+    <>
+      <nav>
+        <button onclick={prevPage} disabled={page === 1}>
+          Prev Page
+        </button>
+        <button onclick={nextPage} disabled={!posts.length}>
+          Next Page
+        </button>
+      </nav>
+      {content}
+    </>
+  );
+};
+
+export default Example1;
---
+++ react_pagination/src/PageButton.js
@@ -0,0 +1,5 @@
+﻿const PageButton = ({ pg, setPage }) => {
+  return <button onClick={() => setPage(pg)}>{pg}</button>;
+};
+
+export default PageButton;
